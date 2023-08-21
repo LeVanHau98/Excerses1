5a20efa27873b12e287ee52177c6d80d91fe43ed
LeVanHau98 <139059526+LeVanHau98@users.noreply.github.com>
2023-08-21 20:03:07 +0700

    Delete EX1 directory

--- EX1/Text1.js
+++
@@ -1,8 +0,0 @@
-const searchBtn = document.querySelector('.search-btn');
-const container = document.querySelector('.container');
-
-
-searchBtn.addEventListener('click', function() {
-    console.log(this)
-    this.parentElement.classList.toggle('show');
-})
